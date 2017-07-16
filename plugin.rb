d80dfa83156ad47c7c3919b5b47258d59e0056cc
chriskottom <chris@chriskottom.com>
2017-07-16 15:06:54 +0200

    Initial commit, plugin stub

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: thewinesociety-onebox
+# about: Display Oneboxed product links from The Wine Society online shop
+# version: 0.0.1
+# authors: Chris Kottom
+# url: https://github.com/chriskottom/thewinesociety-onebox
