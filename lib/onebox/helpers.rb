ecdea35ed4972057698ae72b52a0c28de79b8e4d
chriskottom <chris@chriskottom.com>
2017-07-17 10:02:21 +0200

    Use correct logic to determine title

--- lib/onebox/helpers.rb
+++ lib/onebox/helpers.rb
@@ -1,5 +1,22 @@
 module Onebox
   module Helpers
+
+    def self.extract_opengraph(doc)
+      return {} unless doc
+
+      og = {}
+
+      doc.css('meta').each do |m|
+        if (m["property"] && m["property"][/^og:(.+)$/i]) ||
+           (m["name"] && m["name"][/^og:(.+)$/i])
+          value = (m["content"] || m["value"]).to_s
+          og[$1.tr('-:','_').to_sym] ||= value unless Onebox::Helpers::blank?(value)
+        end
+      end
+
+      og
+    end
+
     def self.extract_product_info(doc)
       return {} unless doc
 
